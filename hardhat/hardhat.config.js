6e6f8026fe33f87c18d756ba5b720f2cc054464c
Cossie <wcossie9@gmail.com>
2023-06-23 11:41:07 +0300

    Environment Variables setup

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,2 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+
+module.exports ={
+  solidity:"0.8.18",
+  networks:{
+    sepolia:{
+      url:process.env.RPC_URL,
+      accounts:[process.env.PRIVATE_KEY],
+
+    },
+  },
+  etherscan:{
+    apikey:process.env.ETHERSCAN_API_KEY,
+  },
+
+
+};
