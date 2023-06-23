40d72b9c104d43fcba70c4038b501b6d2b2e7227
Cossie <wcossie9@gmail.com>
2023-06-23 12:24:34 +0300

    Create ERC20 Token

--- hardhat/contracts/Token.sol
+++ hardhat/contracts/Token.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract token is ERC20{
+contract Token is ERC20{
     //Initialize contract with 1million Tokens
     constructor() ERC20("Token", "TKN"){
         _mint (msg.sender,1000000 * 10 ** decimals());
     }
 
 }
-
 
