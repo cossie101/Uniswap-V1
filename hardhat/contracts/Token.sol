5c38ea855419e191920bbc265d146ed5e86fa706
Cossie <wcossie9@gmail.com>
2023-06-23 11:49:20 +0300

    ERC20 Token Created

---
+++ hardhat/contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract token is ERC20{
+    //Initialize contract with 1million Tokens
+    constructor() ERC20("Token", "TKN"){
+        _mint (msg.sender,1000000 * 10 ** decimals());
+    }
+
+
+}
+
+
